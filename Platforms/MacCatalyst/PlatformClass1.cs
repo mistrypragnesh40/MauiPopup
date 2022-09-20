f87831e6f15e3dfdf6eceb81d52730fc6855c5ae
Pragnesh Mistry <mistrypragnesh40@gmail.com>
2022-09-20 11:52:13 +0530

    Add project files.

---
+++ Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiPopup
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ PopupAction.cs
@@ -0,0 +1,21 @@
+﻿using MauiPopup.Views;
+
+namespace MauiPopup
+{
+    // All the code in this file is included in all platforms.
+    public class PopupAction
+    {
+        public static async Task DisplayPopup(BasePopupPage page)
+        {
+            if (Application.Current.MainPage != null)
+                await Application.Current.MainPage.Navigation.PushModalAsync(page);
+        }
+
+        public static async Task ClosePopup()
+        {
+            if (Application.Current.MainPage != null)
+                await Application.Current.MainPage.Navigation.PopModalAsync();
+        }
+
+    }
+}
